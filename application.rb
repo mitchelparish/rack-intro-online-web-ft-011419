48100073eb93cf589b088f270ad2987929424cea
Mitchel Parish <mitchelparish@gmail.com>
2019-02-20 15:49:26 +0000

    Automatically backed up by Learn

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World!! My name is Mitch!!"
+    resp.write "Hello, my name is Mitch!!"
     resp.finish
   end
 
